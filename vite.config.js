6670edeb84edefe277f69f5050350469fcdf9a5e
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2024-03-13 22:56:09 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), ViteCSSExportPlugin()],
 	
-	base: "/curriculo",
 	build: {
 		target: 'esnext' //browsers can handle the latest ES features
 	}
